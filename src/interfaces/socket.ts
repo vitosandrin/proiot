5d562c40d90b101d12954f655802cfd07877676c
Vito <sandrinvito10@gmail.com>
2023-02-22 00:55:49 -0300

    fix: remove unused interface

--- src/interfaces/socket.ts
+++ src/interfaces/socket.ts
@@ -1,10 +1,3 @@
-export interface ISocketData {
-  name: string;
-  description: string;
-  temperature: string;
-  humidity: string;
-}
-
 export interface ISocketProps {
   message: string;
   id: string;
