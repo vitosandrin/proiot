d8229d0266fb84c640ad1d4a0f32d0a5126e47bb
Vito <sandrinvito10@gmail.com>
2023-02-24 02:17:28 -0300

    feat: sendDevices socket event / send all devices to client

--- server/src/ws.ts
+++ server/src/ws.ts
@@ -1,17 +1,14 @@
 import { Server as HttpServer } from "http";
 import { Socket, Server } from "socket.io";
 import { DeviceModel } from "./models/device";
-import Service from "./services/Service";
 
 export class ServerSocket {
   public static instance: ServerSocket;
   public io: Server;
-  public device;
 
   constructor(server: HttpServer) {
     ServerSocket.instance = this;
-    const device = Service(DeviceModel);
-    this.device = device;
+
     this.io = new Server(server, {
       pingInterval: 10000,
       pingTimeout: 5000,
@@ -25,7 +22,7 @@ export class ServerSocket {
   }
 
   connect = (socket: Socket) => {
-    console.info("Connection received from " + socket.id);
+    console.info("Connection received from ", socket.id);
 
     socket.on("sendDevice", async (data) => {
       console.log(data, "from", socket.id);
@@ -33,8 +30,14 @@ export class ServerSocket {
       socket.broadcast.emit("receiveDevice", device);
     });
 
+    socket.on("sendDevices", async (data) => {
+      console.log(data, "from", socket.id);
+      const devices = await DeviceModel.find();
+      socket.broadcast.emit("receiveDevices", devices);
+
+    })
     socket.on("disconnect", () => {
-      console.info("Disconnect received from: " + socket.id);
+      console.info("Disconnect received from: ", socket.id);
     });
   };
 }
