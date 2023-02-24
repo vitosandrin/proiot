90c6959475bdf1df5ffb4cfd0a47d36fdbf00e6f
Vito <sandrinvito10@gmail.com>
2023-02-24 02:18:32 -0300

    feat: receiveDevices from socket / button create device

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,56 @@
-import {
-  FlexBox,
-  Input,
-  ListDevice,
-  NewDevice,
-  UpdateDevice,
-} from "./components";
 import { useState, useEffect } from "react";
 import { api } from "./utils/axios";
+import {
+  Button,
+  FlexBox,
+  LastRecord,
+  ListDevice,
+  Modal,
+  NewDevice,
+} from "./components";
+import { theme } from "./theme";
+import { socket } from "./utils/socket";
 import { IDevice } from "./interfaces/device";
 
 function App() {
   const [devices, setDevices] = useState([]);
+  const [modalOpened, setModalOpened] = useState(false);
+  const [devicesReceived, setDevicesReceived] = useState<IDevice[]>();
 
   const getAll = async () => {
     const result = await api.get("/device?limit=50");
     setDevices(result?.data?.data?.result);
   };
+
+  useEffect(() => {
+    socket.on("receiveDevices", (data) => {
+      console.log("devices", data);
+      setDevicesReceived(data);
+    });
+  }, [socket]);
+
   useEffect(() => {
     getAll();
   }, []);
+
   return (
     <FlexBox gap="xs" align="center" justify="space-around" direction="column">
       <ListDevice
-        devices={devices}
+        devices={devicesReceived ? devicesReceived : devices}
         handleRemoveClick={getAll}
         handleUpdateClick={getAll}
       />
-      <NewDevice onAction={getAll} />
+      <Button
+        hoverColor={theme.colors.neutral[1]}
+        backgroundColor={theme.colors.neutral.pure}
+        primaryColor={theme.font.colors.white}
+        text="Create New Device"
+        onClick={() => setModalOpened(true)}
+      />
+      <LastRecord />
+      <Modal open={modalOpened} onClose={() => setModalOpened(false)}>
+        <NewDevice onAction={getAll} />
+      </Modal>
     </FlexBox>
   );
 }
